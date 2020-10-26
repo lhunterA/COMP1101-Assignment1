e58fa49deb1245e2fddf4d76afd64b39e8e8cb2a
lhunterA <200425671@student.georgianc.on.ca>
2020-10-26 18:58:49 -0400

    Created Chart and chart controller.

---
+++ src/ProvinceVoterTurnoutController.java
@@ -0,0 +1,20 @@
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+
+public class ProvinceVoterTurnoutController {
+
+    @FXML
+    private BarChart<String, Double> electionBarChart;
+
+    @FXML
+    private CategoryAxis provinceXAxis;
+
+    @FXML
+    private NumberAxis avgYAxis;
+
+
+
+
+}
