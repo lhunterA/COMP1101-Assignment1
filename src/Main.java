aeda698798144bbde7d9733d98f329c3cb061a9b
lhunterA <200425671@student.georgianc.on.ca>
2020-10-27 17:15:03 -0400

    Adding an Icon to the Fx Stage

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application
@@ -34,7 +35,7 @@ public class Main extends Application
          Scene scene = new Scene(root);
          primaryStage.setScene(scene);
          primaryStage.setTitle("2019 Canadian Election");
-         //primaryStage.getIcons();  //commented out until Styling is done.
+         primaryStage.getIcons().add(new Image("icon.png"));
          primaryStage.show();
 
     }
